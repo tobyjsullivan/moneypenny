7b943b58d15a9ffa5ee803c12cb199fa0633ba82
Toby Sullivan <tobyjsullivan@gmail.com>
2017-04-23 23:03:50 -0700

    Fetch live weather updates

--- updates/handler.go
+++ updates/handler.go
@@ -1,21 +1,31 @@
 package updates
 
-import "bytes"
+import (
+    "bytes"
+    "time"
+    "github.com/tobyjsullivan/moneypenny/weather"
+)
 
 func BuildResponse() string {
-    return compileUpdate().String()
+    return compileUpdate().String() + "That's all for today. See you tomorrow."
 }
 
 func compileUpdate() *update {
-    return &update{
+    fGreeting := asyncMatter(greeting)
+    fWeather := asyncMatter(weatherUpdate)
+    fCalendar := asyncMatter(calendar)
+    fReminder := asyncMatter(reminders)
+
+    out := &update{
         matters: []*matter{
-            {text: "Good afternoon Toby."},
-            {text: "It's looking sunny outside - you can leave the umbrella at home."},
-            {text: "You have the day off tomorrow with no appointments."},
-            {text: "Remember to send Jess some sweet texts."},
-            {text: "That's all for today. See you tomorrow."},
+            <- fGreeting,
+            <- fWeather,
+            <- fCalendar,
+            <- fReminder,
         },
     }
+
+    return out
 }
 
 type update struct {
@@ -33,4 +43,83 @@ func (u *update) String() string {
 
 type matter struct {
     text string
+}
+
+func asyncMatter(f func() *matter) chan *matter {
+    var c = make(chan *matter)
+    go func() {
+        res := f()
+        c <- res
+    }()
+
+    return c
+}
+
+func greeting() *matter {
+    loc, _ := time.LoadLocation("America/Vancouver")
+    hour := time.Now().In(loc).Hour()
+    if hour < 12 {
+        return &matter{text: "Good morning Toby"}
+    } else if hour < 18 {
+        return &matter{text: "Good afternoon Toby"}
+    } else {
+        return &matter{text: "Good evening Toby"}
+    }
+}
+
+func weatherUpdate() *matter {
+    fc, err := weather.VancouverForecast()
+    if err != nil {
+        println("ERROR: "+err.Error())
+        return &matter{text: "I encountered an error while trying to check the weather."}
+    }
+
+    var drizzle bool
+    var rain bool
+    var snow bool
+    var thunderstorm bool
+    var extreme bool
+
+    forecastHorizon := time.Now().Add(24 * time.Hour)
+
+    for _, i := range fc.Items {
+        if i.Time.After(forecastHorizon) {
+            continue
+        }
+
+        switch i.Condition {
+        case weather.ConditionDrizzle:
+            drizzle = true
+        case weather.ConditionRain:
+            rain = true
+        case weather.ConditionSnow:
+            snow = true
+        case weather.ConditionThunderstorm:
+            thunderstorm = true
+        case weather.ConditionExtreme:
+            extreme = true
+        }
+    }
+
+    if extreme {
+        return &matter{text: "Be careful outside, there is extreme weather in the forecast. Check weather alerts for Vancouver."}
+    } else if thunderstorm {
+        return &matter{text: "It looks like there's a thunderstorm today - take your umbrella and plan to stay indoors."}
+    } else if snow {
+        return &matter{text: "It looks like there will be some snow today - plan accordingly."}
+    } else if rain {
+        return &matter{text: "It is going to be a rainy day today - take your umbrella."}
+    } else if drizzle {
+        return &matter{text: "It is going to drizzle a bit today - it might be smart to take your umbrella with you."}
+    }
+
+    return &matter{text: "It's looking clear outside today - you can leave the umbrella at home."}
+}
+
+func calendar() *matter {
+    return &matter{text: "I don't have access to your calendar yet so you'll have to check it yourself."}
+}
+
+func reminders() *matter {
+    return &matter{text: "Remember to send Jess some sweet texts."}
 }
---
+++ weather/weather.go
@@ -0,0 +1,114 @@
+package weather
+
+import (
+    "time"
+    "net/http"
+    "fmt"
+    "encoding/json"
+    "os"
+)
+
+const (
+    ConditionClear = "Clear"
+    ConditionClouds = "Clouds"
+    ConditionRain = "Rain"
+    ConditionDrizzle = "Drizzle"
+    ConditionThunderstorm = "Thunderstorm"
+    ConditionSnow = "Snow"
+    ConditionExtreme = "Extreme"
+    ConditionOther = "Other"
+
+    cityVancouver = 6173331
+)
+
+var owmAppId string
+
+func init() {
+    owmAppId = os.Getenv("OPEN_WEATHER_MAP_APPID")
+}
+
+type Forecast struct {
+    Items []*ForecastItem
+}
+
+type ForecastItem struct {
+    Time time.Time
+    Condition string
+}
+
+func VancouverForecast() (*Forecast, error) {
+    url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/forecast?id=%d&appid=%s", cityVancouver, owmAppId)
+    resp, err := http.Get(url)
+    if err != nil {
+        return nil, err
+    }
+
+    decoder := json.NewDecoder(resp.Body)
+    var fc owmForecastResponse
+    err = decoder.Decode(&fc)
+    if err != nil {
+        return nil, err
+    }
+
+    return mapOwmRespToForecast(&fc), nil
+}
+
+type owmForecastResponse struct {
+    List []*owmLine `json:"list"`
+}
+
+type owmLine struct {
+    DateUnix int `json:"dt"`
+    Weather []*owmWeather `json:"weather"`
+    Rain *owmRain `json:"rain"`
+}
+
+type owmWeather struct {
+    ConditionId int `json:"id"`
+}
+
+type owmRain struct {
+    Amount3h float32 `json:"3h"`
+}
+
+func mapOwmRespToForecast(r *owmForecastResponse) *Forecast {
+    items := make([]*ForecastItem, len(r.List))
+
+    for i, li := range r.List {
+        dt := time.Unix(int64(li.DateUnix), 0)
+        cond := ConditionClear
+        if len(li.Weather) > 0 {
+            cond = mapOwmConditionCodeToCondition(li.Weather[0].ConditionId)
+        }
+
+        items[i] = &ForecastItem{
+            Time: dt,
+            Condition: cond,
+        }
+    }
+
+    return &Forecast{
+        Items: items,
+    }
+}
+
+func mapOwmConditionCodeToCondition(code int) string {
+    switch {
+    case code >= 200 && code < 300:
+        return ConditionThunderstorm
+    case code >= 300 && code < 400:
+        return ConditionDrizzle
+    case code >= 500 && code < 600:
+        return ConditionRain
+    case code >= 600 && code < 700:
+        return ConditionSnow
+    case code == 800:
+        return ConditionClear
+    case code > 800 && code < 900:
+        return ConditionClouds
+    case code >= 900 && code < 910:
+        return ConditionExtreme
+    default:
+        return ConditionOther
+    }
+}
